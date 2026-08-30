1217ccaf5a811fcbcdb7e71dee03d23ec38b15b8
kbromfield72 <87086549+kbromfield72@users.noreply.github.com>
2026-08-30 17:52:14 +0100

    Update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'receipt-vault-v3';
+const CACHE = 'receipt-vault-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
